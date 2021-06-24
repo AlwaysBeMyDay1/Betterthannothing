c0aaed2134d4982947c99ecceded414c9e7f28d7
Hyunjik Lee <jiggy0429@gmail.com>
2021-06-25 00:12:53 +0900

    Convert User primary key from Integer to UUID

--- common/models.py
+++ common/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.deletion import CASCADE
-
-
-class Character(models.Model):
-    pass
+import uuid
 
 
 class User(AbstractUser):
@@ -14,6 +11,7 @@ class User(AbstractUser):
     GENDER_FEMALE = 1
     GENDER_UNKNOWN = 2
     GENDER_CHOICES = [(GENDER_MALE, '남성'), (GENDER_FEMALE, '여성'), (GENDER_UNKNOWN, '숨기기')]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, serialize=True)
     name = models.CharField(blank=False, null=False, max_length=100)
     nickname = models.CharField(blank=False, null=False, max_length=100)
     gender = models.IntegerField(choices=GENDER_CHOICES, blank=False, null=False, default=3)
@@ -21,15 +19,11 @@ class User(AbstractUser):
     mileage = models.PositiveIntegerField(default=0)
     level = models.PositiveSmallIntegerField(default=1)
     content = models.TextField(default='소개글이 없습니다.')
-    # character = models.OneToOneField(Character, on_delete=CASCADE)
 
 
-class Profile(models.Model):
-    # 유저와 1:1 대응
-    user = models.OneToOneField(User, on_delete=CASCADE)
-    # 유저의 소개글
-    content = models.TextField()
-    # 이후 유저의 사진이나 링크, 참여 내역 등을 추가 가능
+class Character(models.Model):
+    # user = models.OneToOneField(User, on_delete=CASCADE)
+    pass
 
 
 class Rank(models.Model):
